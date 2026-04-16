bb30c0717bb4d0a2ff1539ab1a04712ac2205d52
muktharcs99 <mohdmuktharcs99@gmail.com>
2026-04-16 10:49:53 +0530

    commit

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
+# from app import create_app
+
+# app = create_app()
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+import eventlet
+eventlet.monkey_patch()
+
 from app import create_app
+from flask_socketio import SocketIO
 
 app = create_app()
+socketio = SocketIO(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
